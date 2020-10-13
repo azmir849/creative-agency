7c4b66a18886e4e32618accde4f7d88ba8c60bc8
azmir849 <azmir15-849@diu.edu.bd>
2020-10-14 02:23:23 +0600

    serviceSponsors added

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
+import ServiceSponsors from '../ServiceSponsors/ServiceSponsors';
 
 
 const Home = () => {
     return (
         <div>
           <Header></Header>
+          <ServiceSponsors></ServiceSponsors>
         </div>
     );
 };
---
+++ src/Components/Home/ServiceSponsors/ServiceSponsors.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './ServiceSponsors.css'
+import slack from '../../../images/logos/slack.png'
+import google from '../../../images/logos/google.png'
+import uber from '../../../images/logos/uber.png'
+import netflix from '../../../images/logos/netflix.png'
+import airbnb from '../../../images/logos/airbnb.png'
+
+const ServiceSponsors = () => {
+    return (
+        <div className="mr-5 d-flex justify-content-center SponsorsContent">
+            <img src={slack} alt=""/>
+            <img src={google} alt=""/>
+            <img src={uber} alt=""/>
+            <img src={netflix} alt=""/>
+            <img src={airbnb} alt=""/>
+        </div>
+    );
+};
+
+export default ServiceSponsors;
