4de8c5657c27b4fefc8b3549699f26f2a4cb023e
azmir849 <azmir15-849@diu.edu.bd>
2020-10-14 15:03:25 +0600

    service added to server

--- src/Components/Admin/AdminPages/AddService/AddService.js
+++ src/Components/Admin/AdminPages/AddService/AddService.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddService.css'
 import { Link } from 'react-router-dom';
 import LOGO from '../../../../images/logos/logo.png'
 
 const AddService = () => {
+    const[info, setInfo] = useState({});
+    const[file, setFile] = useState(null);
+
+    const handleBlur = e => {
+        const newInfo = { ...info };
+        newInfo[e.target.name] = e.target.value;
+        setInfo(newInfo);
+        console.log(newInfo)
+    }
+
+    const handleFileChange = (e) => {
+        const newFile = e.target.files[0];
+        setFile(newFile);
+        console.log(newFile)
+    }
+
+    const handleSubmit = () => {
+        const formData = new FormData()
+        console.log(info);
+        formData.append('file', file);
+        formData.append('title', info.title);
+        formData.append('description', info.description);
+
+        fetch('http://localhost:5000/services', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -17,15 +53,21 @@ const AddService = () => {
                   <h6 className="">Add event</h6>
                   <h6 className="d-flex AdminName justify-content-end">Sufi Ahamed</h6>
             
-                <div className="servicePostArea">
-                    <h6 className="postContent">Service Title</h6>
-                    <input className="postContent" type="text" name="title" id="" placeholder="Enter title"/>
-                    <h6 className="postContent">Icon</h6>
-                    <button className="postContent UploadButton">Upload image</button>
-                    <h6 className="postContent">Description</h6>
-                    <textarea className="postContent" name="description" id="" cols="40" rows="3" placeholder="Enter Designation"></textarea>
-                    <button className="btn-success submitButton">Submit</button>
-                 </div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="title">Enter Title</label>
+                        <input onBlur={handleBlur} type="text" className="form-control" name="title" placeholder="Enter title" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="image">Icon</label>
+                        <input onChange={handleFileChange} type="file" className="form-control" id="image" placeholder="Upload image" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="title">Description</label>
+                        <input onBlur={handleBlur} type="text" className="form-control" name="description" placeholder="Enter Designation" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
                </div>
             </div>    
         </div>
