cc51a72473b215f31ced7d6d6ccc12caa225b587
azmir849 <azmir15-849@diu.edu.bd>
2020-10-16 11:55:11 +0600

    jwt install applied on private route

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useContext } from 'react';
-import { UserContext } from '../../../App';
-import jwt_decode from "jwt-decode";
+import JwtDecode from 'jwt-decode';
+import React, { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { UserContext } from '../../App';
 
-const PrivateRoute = ({ children, ...rest }) => {
-  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
-  const isLoggedIn = () => {
-    const token = sessionStorage.getItem('token');
-    if (!token) {
-      return false;
+const PrivateRoute = ({children, ...rest}) => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    
+    const isLoggedIn = () => {
+      const token = sessionStorage.getItem('token');
+      if(!token){
+        return false;
+      }
+      const decodedToken = JwtDecode(token);
+      // get current time
+      const currentTime = new Date().getTime() / 1000;
+      // compare the expiration time with the current time
+      // will return false if expired and will return true if not expired
+      return decodedToken.exp > currentTime;
     }
-    const decodedToken = jwt_decode(token);
-    // get current time
-    const currentTime = new Date().getTime() / 1000;
-    // compare the expiration time with the current time
-    // will return false if expired and will return true if not expired
-    return decodedToken.exp > currentTime;
-  }
-  return (
-    <Route
+    return (
+        <Route
       {...rest}
       render={({ location }) =>
         (loggedInUser.email || isLoggedIn()) ? (
           children
         ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-          )
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
       }
     />
-  );
+    );
 };
 
 export default PrivateRoute;
